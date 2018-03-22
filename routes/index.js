4d24583110ad89b789af07b64b1dad1db331adb9
aquinovo <vela.aquino@gmail.com>
2018-03-22 04:09:04 -0600

    upload file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var multipart = require('connect-multiparty');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+home=path.resolve("."); 
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET home page. */
+router.post('/upload',multipart(), function(req, res) {
+
+    var file = req.files.fileCSV,
+    name = file.name,
+    type = file.type,
+    ruta = home + "/public/archivos/";
+
+    console.log(name,type,ruta);
+    fs.rename(file.path, ruta, function(err){
+        if(err) res.send("Ocurrio un error al intentar subir la imagen");
+    });
+
+
+    res.send("upladed file");
+    
+});
+
+
 
 module.exports = router;
